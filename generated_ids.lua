0e6c2f85c59734d9a7de49a1c0a9002dc366df19
Minotorious <minotorious@gmail.com>
2020-12-31 00:01:12 +0100

    new pagoda

---
+++ generated_ids.lua
@@ -0,0 +1,15 @@
+-- This is a generated file
+-- Do not modify it, unless you know exactly what you are doing
+
+ModId = "a70c5a09-dd6d-4756-b195-434c3e780795"
+
+IdMap = {
+	{ "PAGODA_1", "BUILDING", "5c0efda7-4c92-498c-95b5-023421a30b6f" },
+	{ "models/BisonPlains.obj", "", "98434c5e-995c-45c2-a8e6-01cf1f64419a" },
+	{ "models/kuaSRF.jpg", "", "ab47dac8-33d8-4b53-a418-5ffbe9eb4ada" },
+	{ "PAGODA_1_PART", "BUILDING_PART", "25eb5e6f-816b-4d77-9caf-7dff913cde77" },
+	{ "models/BisonPlains_BaseColor.png", "", "9e4c48f7-c4e7-44fc-a82d-dd4dc3b26fd9" },
+	{ "models/pagoda.fbx", "", "a743cbe2-fbf4-42ae-9bf8-27fd0907b653" },
+	{ "models/redscale.png", "", "9d2f375e-41c5-4acd-b167-7d05ad8c0df7" },
+	{ "models/WciR8Z.jpg", "", "332442b0-daa5-44e8-89e6-fd1576ce3778" },
+}
---
+++ mod.lua
@@ -0,0 +1,65 @@
+--[[---------------------------------------------------------------------------\
+| ||\\    //||       /|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\ |
+| || \\  // ||  (o_ / |                     MAIN MOD FILE                    | |
+| ||  \\//  ||  //\/  |                         ----                         | |
+| ||   \/   ||  V_/_  |                      LUNAR REVEL                     | |
+| ||        ||        |‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗/ |
+\---------------------------------------------------------------------------]]--
+
+local lunarRevel = foundation.createMod();
+
+lunarRevel:log("Lunar Revel Mod Loading")
+
+--[[---------------------------- GENERAL MATERIALS ----------------------------]]--
+
+lunarRevel:registerAssetId("models/pagoda.fbx/Materials/Material.RedScales", "MATERIAL_SCALES_RED")
+
+lunarRevel:override({
+    Id = "MATERIAL_SCALES_RED",
+    --IsLighted = false
+})
+
+--[[------------------------------ MAIN MONUMENT ------------------------------]]--
+
+lunarRevel:registerAssetId("models/pagoda.fbx/Prefab/PagodaPart", "PREFAB_PAGODA_1_PART")
+
+lunarRevel:register({
+	DataType = "BUILDING",
+	Id = "PAGODA_1",
+	Name = "PAGODA_1_NAME",
+	Description = "PAGODA_1_DESC",
+	BuildingType = "DECORATION",
+	AssetCoreBuildingPart = "PAGODA_1_PART"
+})
+
+lunarRevel:register({
+	DataType = "BUILDING_PART",
+	Id = "PAGODA_1_PART",
+    Name = "PAGODA_1_PART_NAME",
+	Description = "PAGODA_1_PART_DESC",
+	ConstructorData = {
+		DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+		CoreObjectPrefab = "PREFAB_PAGODA_1_PART"
+	},
+	BuildingZone = {
+		ZoneEntryList = {
+            {
+			Polygon = polygon.createCircle( 10 , { 0, 0 } , 36 ),
+			Type = { DEFAULT = true, NAVIGABLE = false, GRASS_CLEAR = true }
+			}
+        }
+	},
+	ConstructionVisual = nil,
+	Cost = {
+		RessourcesNeeded = {}
+	},
+    IsVisibleWhenBuilt = true
+})
+
+
+
+--[[------------------------------- MAIN EVENT --------------------------------]]--
+
+--[[-------------------------------- DO FILES ---------------------------------]]--
+
+--lunarRevel:dofile("scripts/alteredTextures.lua")
